a5d856978e5edd5d3022edfdf8bcfd6ba26bbb89
Ernest Lipford <Ernest.Lipford@resapower.com>
2023-12-05 09:16:07 -0600

    added sign up form html

--- src/auth/signup.jsx
+++ src/auth/signup.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from 'react';
-
+import './signup.style.css';
+//import {createPlayer} from '../services/playersvc'
 function SignUp() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -9,12 +10,31 @@ function SignUp() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    createPlayer(firstName,lastName,email,password);
+ 
     console.log(`First Name: ${firstName}, Email: ${email}`);
   };
 
   return (
-   <div><h1>This is the sign up page</h1></div>
+    <div className="signup_container">
+    <div className="wrapper">
+      <div className="title"><span>Login Form</span></div>
+      <form action="#">
+        <div className="row">
+          <i className="fas fa-user"></i>
+          <input type="text" placeholder="Email or Phone" required/>
+        </div>
+        <div className="row">
+          <i className="fas fa-lock"></i>
+          <input type="password" placeholder="Password" required/>
+        </div>
+        <div className="pass"><a href="#">Forgot password?</a></div>
+        <div className="row button">
+          <input type="submit" value="Login"/>
+        </div>
+        <div className="signup-link">Not a member? <a href="#">Signup now</a></div>
+      </form>
+    </div>
+  </div>
   );
 }
 export default SignUp
