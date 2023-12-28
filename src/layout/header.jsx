2486750bf078f45e5d579db033d29bc00948d1bd
Ernest Lipford <ernestlipford@Ernests-MacBook-Air.local>
2023-12-28 09:19:24 -0600

    added context to signup and word for username, score, and attempt numbers.

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export function Header() {
 
     return (
-        <div className="header">
-            <h1>Word Bang</h1>    </div>
+       <header>
+             <h1>Word Bang</h1>  
+       </header>
+             
     );
 
 }
--- src/layout/left_sidebar.jsx
+++ src/layout/left_sidebar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export function LeftSidebar(){
     return (
-        <div className="sidebar-1">
+        <nav>
        
                 <ul>
                     <li><Link to="/word">Go to game</Link></li>
                         <li><Link to="/signup">Sign up</Link></li>
                     </ul>
      
-        </div>
+        </nav>
         
     );
 }
