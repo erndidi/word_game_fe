8fa331ca92b74e7120dc9ace457324464a538e92
erndi <erndidi@gmail.com>
2024-01-02 13:26:54 -0600

    changes before meeting

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export function App () {
                              <LeftSideBar/>
                         <MainContent/>
                         <RightSidebar/>
-                            
+                            <Footer></Footer>
                       
                        
                     </div>
