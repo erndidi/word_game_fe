a6d23fe87b0bd3e59be0b4a94bdbdd6f23d55634
Ernest Lipford <ernestlipford@Ernests-MacBook-Air.local>
2024-02-06 11:06:39 -0600

    trying css grid ui to see if I can get better result

--- src/App.jsx
+++ src/App.jsx
@@ -24,40 +24,29 @@ export function App () {
         return (
             <PlayerProvider value={{username:"player1", sessionid: "0000000", score:0} }>
                     <BrowserRouter>
-                    <div className="container">
-                        <header className='flex-header'>
-                            <Header/>
-                        </header>
-                        <main className='flex-main'>
-                            <nav className='flex-nav'>
+                    <div class="holy-grail-grid">
+            <header class="header"> <Header/></header>
+            <main class="main-content">    
+                <Routes>
+                     <Route path="/" element={<Word />} />
+                        <Route path="/word" element={<Word />} />ß
+                        <Route path="/signup" element={<SignUp />} />    ß
+                        <Route path="/login" element={<Login />} />   
+                        <Route path="/logout" element={<Logout />} />     
+                
+            </Routes></main>
+            <section class="left-sidebar"> 
+                    <nav className='game_nav'>
                             <ul>
                                 <li><Link to="/word">Go to game</Link></li>
                                     <li><Link to="/signup">Sign up</Link></li>
                                     <li><Link to="/login">Login</Link></li>
                                     <li><Link to="/logout">Logout</Link></li>
                             </ul>
-                            </nav>
-                        <article className='flex-article'>
-                        <Routes>
-            <Route path="/" element={<Word />} />
-            <Route path="/word" element={<Word />} />
-                <Route path="/signup" element={<SignUp />} />    
-                <Route path="/login" element={<Login />} />   
-                <Route path="/logout" element={<Logout />} />      
-                
-            </Routes>
-                        </article>
-                        <aside className='flex-aside'>
-                        <RightSidebar/>
-
-                        </aside>
-                        </main>  
-                        <footer className='flex-footer'>
-                            <Footer></Footer>
-                        </footer>
-                      
-                       
-                    </div>
+                            </nav></section>
+            <aside class="right-sidebar"><RightSidebar/></aside>
+            <footer class="footer">Footer</footer>
+        </div>
                        </BrowserRouter>
                        
              </PlayerProvider>
